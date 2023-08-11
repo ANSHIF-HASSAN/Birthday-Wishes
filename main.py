39c60cbf3e98598f40be887b8dc71734ae66e71b
ANSHIF.MP4 <104834177+Anshif-Hassan@users.noreply.github.com>
2023-08-11 13:07:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ wish = "Hey {} ! Happy Birthday! May god bless you <3\n"
 name = input(promptName)
 print(wish.format(name))
 time.sleep(2)
-print("Follow Anshif-Hassan @ github for more! ;)")
+print("Follow Anshif-Hassan @ github for more!")
 p = "POWERED"
 by = "BY"
 u = "@Anshif-Hassan"
