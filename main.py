bccf3d97b455e7bcdc9a659ec00cf28df0f99a10
ANSHIF.MP4 <104834177+Anshif-Hassan@users.noreply.github.com>
2023-08-11 08:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import time
 
-promptName = "What's your name? :)\n"
+promptName = "What's your sweet name? :)\n"
 wish = "Hey {} ! Happy Birthday! May god bless you <3\n"
 name = input(promptName)
 print(wish.format(name))
 time.sleep(2)
-print("Follow AbhiDevOfficial @ github for more! ;)")
+print("Follow Anshif-Hassan @ github for more! ;)")
 p = "POWERED"
 by = "BY"
-u = "@AbhiDevOfficial"
+u = "@Anshif-Hassan"
 for letter in p:
     time.sleep(0.5)
     print(letter)
